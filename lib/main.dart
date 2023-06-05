986b2484d8699580fd413af2c483e5148d934428
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-06-05 10:18:23 -0300

    branch teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_auth/firebase_options.dart';
 
-import 'home_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MainApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
 }
