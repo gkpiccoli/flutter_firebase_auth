7fddd9963ae659b51d44dc9dd4eb29b58d178667
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-06-05 13:59:18 -0300

    add firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
+  // FALTA ADICIONAR O FIREBASE AUTH AQUI!!!!
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(debugShowCheckedModeBanner: false,
