2141bd7a99c52c391a06a7fd2314afa4f53f73d0
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-06-05 12:55:16 -0300

    Adicionado os campos login e senha

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,6 +11,55 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return ();
+    return  Scaffold(
+      appBar: AppBar(title: const Text('Firebase Login')),
+      body:  SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(color: Colors.black,
+                )
+              ),
+              width: MediaQuery.of(context).size.width * .7,
+              child: const TextField(
+                style: TextStyle(color: Colors.black, fontSize: 20),
+                decoration: InputDecoration(
+                  hintText: 'Digite seu e-mail',
+                  hintStyle:TextStyle(color: Colors.black, fontSize: 16),
+                  border: InputBorder.none
+                ),
+
+
+              ),
+            ),
+          ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border.all(color: Colors.black,
+                    )
+                ),
+                width: MediaQuery.of(context).size.width * .7,
+                child: const TextField(
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: InputDecoration(
+                      hintText: 'Digite sua senha',
+                      hintStyle:TextStyle(color: Colors.black, fontSize: 16),
+                      border: InputBorder.none
+                  ),
+
+
+                ),
+              ),
+            ),
+          ]
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(debugShowCheckedModeBanner: false,
+    home: LoginPage());
   }
 }
